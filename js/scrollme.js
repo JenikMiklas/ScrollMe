814e3d9a546767cc1d1ba3d3c968922ec6b9d238
JenikMiklas <jenikmiklas@gmail.com>
2021-04-17 22:57:31 +0200

    current page tracking added

--- js/scrollme.js
+++ js/scrollme.js
@@ -1,12 +1,17 @@
 class ScrollMe {
 
-    //PUBLIC atributy
+    //PUBLIC
     currentPage = 1 //aktuální stránka
     duration = 5000 //rychlost animace scrollování
-    #sectionNodes
-    #navigationNodes
+
+    //PRIVATE
+    #sectionNodes   // pole sekcí (stránek)
+    #navigationNodes // pole navigačních elementů
+
+    //CLASS PRIVATE
     static #Animating = false
 
+    //init()
     constructor(sectionNodes, navNodes) {
         this.#sectionNodes = sectionNodes
         this.#navigationNodes = navNodes
@@ -14,8 +19,10 @@ class ScrollMe {
     }
     
     #scrollToo(section) {
-        // element sekce stránky .page
+        // element sekce stránky
         let target = section
+        this.currentPage = this.#sectionNodes.indexOf(section) + 1
+        console.log(this.currentPage)
         // získá y souřadnici daného elementu
         let targetPosition = target.getBoundingClientRect().top
         // hodnota aktuální souřadnice y (kde se zrovna nacházím při scrollování stránky)
@@ -26,6 +33,7 @@ class ScrollMe {
         let duration = this.duration
         let startTime = null
 
+        // funkce se vykonává dokola dokud animace neskončí
         function animation(currentTime) {
             //console.log(global.animating)
             if(startTime === null) startTime = currentTime
@@ -50,13 +58,15 @@ class ScrollMe {
         console.log("ANIMATION STARTED")
         requestAnimationFrame(animation)
     }
-
+    
+    // prováže navigační prvky a sekce, nastavení listenerů včech navigačních prvků na akci click
     #initEventListernes() {
         let navigations = []
         let sections = []
         document.querySelectorAll(this.#sectionNodes).forEach(section => {
             sections.push(section)
         })
+        this.#sectionNodes = sections
         this.#navigationNodes.forEach(navTxt => {
             navigations.push(document.querySelectorAll(navTxt))
         })
